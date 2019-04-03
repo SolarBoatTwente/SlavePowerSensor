143d3a39abdec419aca9d1b04426ff4cf32519b8
Rode_Niels <n.leijen@student.utwente.nl>
2019-04-03 23:33:14 +0200

    updated documentation to draft version

--- Software/SPSI/SBT_SPSI.ino
+++ Software/SPSI/SBT_SPSI.ino
@@ -47,111 +47,110 @@ void setup() {
 	initSucces ?
 			Serial.println("CAN initialization ok.") :
 			Serial.println("CAN initialization failed.");
-}
 #endif
 }
 
 void loop() {
-if (mcp2515_check_message()) {
-	tCAN msg;
-	if (mcp2515_get_message(&msg)) {
+	if (mcp2515_check_message()) {
+		tCAN msg;
+		if (mcp2515_get_message(&msg)) {
 #ifdef DEBUG
-		Serial.print("ID: ");
-		Serial.print(msg.id, HEX);
-		Serial.print(", ");
-		Serial.print("Data: ");
-		Serial.print(msg.header.length, DEC);
-		for (int i = 0; i < msg.header.length; i++) {
-			Serial.print(msg.data[i], HEX);
-			Serial.print(" ");
+			Serial.print("ID: ");
+			Serial.print(msg.id, HEX);
+			Serial.print(", ");
+			Serial.print("Data: ");
+			Serial.print(msg.header.length, DEC);
+			for (int i = 0; i < msg.header.length; i++) {
+				Serial.print(msg.data[i], HEX);
+				Serial.print(" ");
+			}
+			Serial.println("");
+#endif
+			// RX command to turn relays of the SPS on or off
+			if (msg.id == CAN_ADDRESS_RELAY) {
+				if (msg.header.length == 2) {
+					// first two bytes (LSB first) have relay status setting info as unsigned integer
+					for (char i = 0; i < 2; i++) {
+						for (char j = 0; j < 8; j++) {
+							// write relay setting to SPS
+							Wire.beginTransmission((I2C_ADDRESS + i * 8 + j));
+							// shorthand for check byte, bitshift byte j positions to the right, check if it is an odd number (=1) and write to I2C
+							Wire.write((msg.data[i] >> j) % 1);
+							Wire.endTransmission();
+						}
+#ifdef DEBUG
+						Serial.print("Sent I2C messages: ");
+						for (int j = 0; j < 2; j++) {
+							Serial.print(msg.data[j], BIN);
+						}
+						Serial.print(" to I2CIDs: ");
+						Serial.print(I2C_ADDRESS, HEX);
+						Serial.print("..");
+						Serial.print(I2C_ADDRESS + 16, HEX);
+#endif
+					}
+				} else if (msg.header.length == 0) {
+					// do a SPS exist check
+					SPSDevices = SPSScan();
+				} // else do nothing with the message
+			} // SPSI does not respond to any other CAN commands
 		}
-		Serial.println("");
-#endif
-		// RX command to turn relays of the SPS on or off
-		if (msg.id == CAN_ADDRESS_RELAY) {
-			if (msg.header.length == 2) {
-				// first two bytes (LSB first) have relay status setting info as unsigned integer
-				for (char i = 0; i < 2; i++) {
-					for (char j = 0; j < 8; j++) {
-						// write relay setting to SPS
-						Wire.beginTransmission((I2C_ADDRESS + i * 8 + j));
-						// shorthand for check byte, bitshift byte j positions to the right, check if it is an odd number (=1) and write to I2C
-						Wire.write((msg.data[i] >> j) % 1);
-						Wire.endTransmission();
-					}
-#ifdef DEBUG
-					Serial.print("Sent I2C messages: ");
-					for (int j = 0; j < 2; j++) {
-						Serial.print(msg.data[j], BIN);
-					}
-					Serial.print(" to I2CIDs: ");
-					Serial.print(I2C_ADDRESS, HEX);
-					Serial.print("..");
-					Serial.print(I2C_ADDRESS + 16, HEX);
-#endif
-				}
-			} else if (msg.header.length == 0) {
-				// do a SPS exist check
-				SPSDevices = SPSScan();
-			} // else do nothing with the message
-		} // SPSI does not respond to any other CAN commands
 	}
-}
 
 // lazy mode; used blinkwithoutdelay example
-unsigned long currentMillis = millis();
-if (currentMillis - previousMillis >= CAN_INTERVAL) {
-	previousMillis = currentMillis;
+	unsigned long currentMillis = millis();
+	if (currentMillis - previousMillis >= CAN_INTERVAL) {
+		previousMillis = currentMillis;
 
-	for (char i = 0; i < 16; i++) {
-		// only send messages to devices present
-		if ((SPSDevices >> i) % 1) {
-			// initialize CAN buffer
-			tCAN msg;
-			msg.id = CAN_ADDRESS_POWER + i;
-			msg.header.rtr = 0;
-			msg.header.length = 8;
+		for (char i = 0; i < 16; i++) {
+			// only send messages to devices present
+			if ((SPSDevices >> i) % 1) {
+				// initialize CAN buffer
+				tCAN msg;
+				msg.id = CAN_ADDRESS_POWER + i;
+				msg.header.rtr = 0;
+				msg.header.length = 8;
 
-			// request 8 bytes from slave device
-			Wire.requestFrom(i + I2C_ADDRESS, 8);
-			for (char j = 0; j < 8; j++) {
-				// save requested data to message buffer (= equal length)
-				msg.data[j] = Wire.read();
-			}
+				// request 8 bytes from slave device
+				Wire.requestFrom(i + I2C_ADDRESS, 8);
+				for (char j = 0; j < 8; j++) {
+					// save requested data to message buffer (= equal length)
+					msg.data[j] = Wire.read();
+				}
 
-			//mcp2515_bit_modify(CANCTRL, (1<<REQOP2)|(1<<REQOP1)|(1<<REQOP0), 0);
-			mcp2515_send_message(&msg);
+				//mcp2515_bit_modify(CANCTRL, (1<<REQOP2)|(1<<REQOP1)|(1<<REQOP0), 0);
+				mcp2515_send_message(&msg);
 
 #ifdef DEBUG
-			Serial.print("Sent CAN message: ");
-			for (int j = 0; j < 8; j++) {
-				Serial.print(msg.data[j], HEX);
-			}
-			Serial.print(" to CANID: ");
-			Serial.println(msg.id, HEX);
+				Serial.print("Sent CAN message: ");
+				for (int j = 0; j < 8; j++) {
+					Serial.print(msg.data[j], HEX);
+				}
+				Serial.print(" to CANID: ");
+				Serial.println(msg.id, HEX);
 #endif
 
+			}
 		}
 	}
-}
 }
 
 unsigned int SPSScan() {
-unsigned int devices = 0;
+	unsigned int devices = 0;
 
-for (char i = 0; i < 16; i++) {
-	Wire.beginTransmission(i + I2C_ADDRESS);
-	// if no fault during transmission occurs (device = present)
-	if (Wire.endTransmission() == 0) {
-		// save the SPS number
-		devices |= 1 << i;
+	for (char i = 0; i < 16; i++) {
+		Wire.beginTransmission(i + I2C_ADDRESS);
+		// if no fault during transmission occurs (device = present)
+		if (Wire.endTransmission() == 0) {
+			// save the SPS number
+			devices |= 1 << i;
+		}
 	}
-}
 
 #ifdef DEBUG
-Serial.print("SPSScan() found devices: ");
-Serial.println(devices, BIN);
+	Serial.print("SPSScan() found devices: ");
+	Serial.println(devices, BIN);
 #endif
 
-return devices;
+	return devices;
 }
